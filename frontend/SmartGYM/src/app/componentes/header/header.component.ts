b783f85990779b7a81c6bd4eeef4e0191af38ab1
jesss635000@gmail.com <jesss635000@gmail.com>
2024-05-05 22:25:38 -0600

    header terminado sin backend

--- frontend/SmartGYM/src/app/componentes/header/header.component.ts
+++ frontend/SmartGYM/src/app/componentes/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  tituloHeader: string = 'titulo';
+  tituloHeader: string = 'bienvenido';
 
   //constructores
+  constructor(
+    private router:Router
+  ){
+
+  }
 
   //botones
+  volver(){
+    window.history.back();
+  }
+
+  cerrarSesion(){
+    console.log('cerrando sesion');
+    this.router.navigateByUrl('ingresar');
+  }
+  //metodos
+  //ocultar boton dependiendo si esta en el home o no:
+  ocultar(): boolean {
+    console.log(this.router.url);
+    return this.router.url === '/c/home';
+  }
 }
